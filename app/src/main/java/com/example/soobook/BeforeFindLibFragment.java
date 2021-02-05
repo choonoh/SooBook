678b29a6e7bd2e1fc589eafa9729e95687a0daac
tmddud20 <syby1422@naver.com>
2021-02-05 14:19:40 +0900

    지도 조촐하게 수정 완료 ..ㅋ

--- app/src/main/java/com/example/soobook/BeforeFindLibFragment.java
+++ app/src/main/java/com/example/soobook/BeforeFindLibFragment.java
@@ -3,16 +3,19 @@ package com.example.soobook;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.NaverMap;
@@ -20,7 +23,7 @@ import com.naver.maps.map.OnMapReadyCallback;
 
 public class BeforeFindLibFragment extends Fragment {
 
-    boolean go_to_map = true;
+    boolean go_to_map = false;
     String user_email, user_UID;
 
     FindLibFragment findLibFragment;
@@ -38,7 +41,10 @@ public class BeforeFindLibFragment extends Fragment {
                              Bundle savedInstanceState) {
         final ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_before_find_lib, container, false);
 
-        ActivityCompat.requestPermissions(getActivity(), PERMISSIONS, PERMISSION_REQUEST_CODE);
+            checkRunTimePermission();
+
+
+//        ActivityCompat.requestPermissions(getActivity(), PERMISSIONS, PERMISSION_REQUEST_CODE);
 
         user_email = getArguments().getString("user_email");
         user_UID = getArguments().getString("user_UID");
@@ -60,16 +66,75 @@ public class BeforeFindLibFragment extends Fragment {
         return rootView;
     }
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    public void onRequestPermissionsResult(int permsRequestCode, @NonNull String[] permissions, @NonNull int[] grandResults) {
+        if (permsRequestCode == PERMISSION_REQUEST_CODE && grandResults.length == PERMISSIONS.length) {
+            boolean check_result = true;
 
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            for (int result : grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+            if (check_result) {
                 go_to_map = true;
-//                naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+                Log.e(this.getClass().getName(), "permission1 -> true");
+            } else {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), PERMISSIONS[0])
+                        || ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), PERMISSIONS[1])) {
 
+                    Toast.makeText(getContext(), "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.", Toast.LENGTH_LONG).show();
+                    getActivity().finish();
+
+                }else {
+                    Toast.makeText(getContext(), "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ", Toast.LENGTH_LONG).show();
+                }
             }
         }
     }
+    void checkRunTimePermission(){
+        int hasFineLocationPermission = ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
+            go_to_map = true;
+            Log.e(this.getClass().getName(), "permission2 -> true");
+        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
+
+            // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), PERMISSIONS[0])) {
+
+                // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
+                Toast.makeText(getContext(), "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+                // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(getActivity(), PERMISSIONS,
+                        PERMISSION_REQUEST_CODE);
+
+
+            } else {
+                // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
+                // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(getActivity(), PERMISSIONS,
+                        PERMISSION_REQUEST_CODE);
+            }
+
+        }
+
+    }
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//
+//        if (requestCode == PERMISSION_REQUEST_CODE) {
+//            if (grantResults.length > 0
+//                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                go_to_map = true;
+////                naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+//
+//            }
+//        }
+//    }
 }
